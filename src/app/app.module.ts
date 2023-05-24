a90311b4212ba0e68d1bdb9b36105a00941db1e4
swton <40786285+swton@users.noreply.github.com>
2023-05-24 20:49:51 +0700

    1st day commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RecordComponent } from './record/record.component';
+import { HistoryComponent } from './history/history.component';
+import { AboutComponent } from './about/about.component';
+import { ReportComponent } from './report/report.component';
+import { MaintabComponent } from './maintab/maintab.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes : Routes = [
+  {path:'',component : HomeComponent},
+  {path:'home',component : HomeComponent},
+  {path:'record',component : RecordComponent},
+  {path:'history',component : HistoryComponent},
+  {path:'about',component : AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecordComponent,
+    HistoryComponent,
+    AboutComponent,
+    ReportComponent,
+    MaintabComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
